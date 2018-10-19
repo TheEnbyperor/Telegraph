fbcc6bcf45fb96a0fb0429512f19cd95c90ba1ae
Q Misell <q@misell.cymru>
2018-10-19 12:09:33 +0100

    deploy monitor

--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,5 @@ HASH=`git log --pretty=format:'%H' -n 1`
 docker build -t evilben/telegraph_smtp:$HASH -f Dockerfile.smtp .
 docker push evilben/telegraph_smtp:$HASH
 cat kubes/smtp.yaml | sed "s/(hash)/$HASH/g" | kubectl apply -f -
-#kubectl -n telegraph rollout status deployment/smtp
+kubectl -n telegraph rollout status deployment/smtp
 
