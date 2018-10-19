0951ea76e917efc0e4cf09719641bc899615e2de
Q Misell <q@misell.cymru>
2018-10-19 10:37:30 +0100

    Print messages received on MQTT

--- telegraph.py
+++ telegraph.py
@@ -1,8 +1,13 @@
 import os
 import json
-import escpos
+import escpos.printer
 import time
 import paho.mqtt.client as mqtt
+
+
+class Context:
+    def __init__(self, printer: escpos.printer.Escpos):
+        self.printer = printer
 
 
 def on_connect(client, userdata, flags, rc):
@@ -14,18 +19,46 @@ def on_connect(client, userdata, flags, rc):
     client.subscribe("printer/print")
 
 
-def on_message(client, userdata, msg):
+def on_message(client, userdata: Context, msg):
     try:
         payload = json.loads(msg.payload)
     except json.decoder.JSONDecodeError:
         return
-    print(payload)
+
+    subject = payload.get("subject")
+    if subject is None:
+        return
+    message = payload.get("message")
+    if message is None:
+        return
+
     formatted_time = time.strftime("%Y-%m-%dT%H:%M:%S")
+    printer = userdata.printer
+
+    # Print subject
+    printer.set()
+    printer.set(double_height=True)
+    printer.textln(subject)
+
+    # Print message time
+    printer.set(bold=True)
+    printer.text("Time: ")
+    printer.set(bold=False)
+    printer.textln(formatted_time)
+
+    # Print message
+    printer.textln("")
+    printer.textln(message)
+    printer.cut()
+    print(printer.output)
 
 
 printer = escpos.printer.Dummy()
+printer.hw("INIT")
 
-client = mqtt.Client(userdata={"printer": printer})
+context = Context(printer)
+
+client = mqtt.Client(userdata=context)
 client.on_connect = on_connect
 client.message_callback_add("printer/print", on_message)
 
