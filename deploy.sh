d32965303eeeb59b8316b45170759ffb043df852
Q Misell <q@misell.cymru>
2018-10-19 11:26:26 +0100

    Kubernetes

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,6 @@ HASH=`git log --pretty=format:'%H' -n 1`
 
 docker build -t evilben/telegraph_smtp:$HASH -f Dockerfile.smtp .
 docker push evilben/telegraph_smtp:$HASH
-#cat kubes/django.yaml | sed "s/(hash)/$HASH/g" | kubectl apply -f -
-#kubectl -n wwfypc rollout status deployment/wwyfpc-django-test
+cat kubes/smtp.yaml | sed "s/(hash)/$HASH/g" | kubectl apply -f -
+#kubectl -n telegraph rollout status deployment/smtp
 
