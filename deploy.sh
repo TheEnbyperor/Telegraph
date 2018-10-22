84e5a49a0cbbec371cb5b863899f50d00769ab59
Q Misell <q@misell.cymru>
2018-10-22 19:28:19 +0100

    IFTTT webhook

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,11 @@
 HASH=`git log --pretty=format:'%H' -n 1`
 
 docker build -t evilben/telegraph_smtp:$HASH -f Dockerfile.smtp .
+docker build -t evilben/telegraph_webhook:$HASH -f Dockerfile.http .
 docker push evilben/telegraph_smtp:$HASH
+docker push evilben/telegraph_webhook:$HASH
 cat kubes/smtp.yaml | sed "s/(hash)/$HASH/g" | kubectl apply -f -
+cat kubes/http.yaml | sed "s/(hash)/$HASH/g" | kubectl apply -f -
 kubectl -n telegraph rollout status deployment/smtp
+kubectl -n telegraph rollout status deployment/http
 
---
+++ telegraph_webhook.py
@@ -0,0 +1,32 @@
+import os
+import paho.mqtt.client as mqtt
+from flask import Flask, g
+
+app = Flask(__name__)
+
+
+def on_connect(client, userdata, flags, rc):
+    """
+    Handles subscribing to topics when a connection to MQTT is established
+    :return: None
+    """
+    print("Connected with result code "+str(rc))
+
+
+@app.route('/print-shopping-list', methods=['GET'])
+def shopping_list_hook():
+    g.client.publish("printer/shopping-list")
+    return "OK"
+
+
+if __name__ == '__main__':
+    client = mqtt.Client()
+    client.on_connect = on_connect
+
+    client.connect(os.getenv("MQTT_SERVER", "172.30.2.3"), 1883, 60)
+    client.loop_start()
+
+    with app.app_context():
+        g.client = client
+
+    app.run(host="0.0.0.0", port=80)
