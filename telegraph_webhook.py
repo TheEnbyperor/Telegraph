a48fd26805fbe51ebece16c18ea4401ab1fd1ec0
Q Misell <q@misell.cymru>
2018-10-22 19:48:05 +0100

    Gunicorn

--- telegraph_webhook.py
+++ telegraph_webhook.py
@@ -3,6 +3,7 @@ import paho.mqtt.client as mqtt
 from flask import Flask
 
 app = Flask(__name__)
+client = mqtt.Client()
 
 
 def on_connect(client, userdata, flags, rc):
@@ -13,6 +14,14 @@ def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
 
 
+@app.before_first_request
+def setup():
+    client.on_connect = on_connect
+
+    client.connect(os.getenv("MQTT_SERVER", "172.30.2.3"), 1883, 60)
+    client.loop_start()
+
+
 @app.route('/print-shopping-list', methods=['GET'])
 def shopping_list_hook():
     client.publish("printer/shopping-list")
@@ -20,10 +29,5 @@ def shopping_list_hook():
 
 
 if __name__ == '__main__':
-    client = mqtt.Client()
-    client.on_connect = on_connect
-
-    client.connect(os.getenv("MQTT_SERVER", "172.30.2.3"), 1883, 60)
-    client.loop_start()
 
     app.run(host="0.0.0.0", port=80)
