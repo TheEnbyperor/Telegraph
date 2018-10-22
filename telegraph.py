dc98ea8d726731285e5c6e72d29d5c722657df4d
Benjamin Misell <q@misell.cymru>
2018-10-22 13:27:49 +0100

    Use weasyprint

--- telegraph.py
+++ telegraph.py
@@ -1,18 +1,26 @@
 import os
 import io
 import json
-import regex
 import typing
 import escpos.constants
 import escpos.printer
 import time
+import math
 import paho.mqtt.client as mqtt
 import struct
-import w3lib.url
-import requests
-from PIL import Image, ImageOps, ImageDraw, ImageFont
-from lxml import etree
-from emoji import UNICODE_EMOJI
+import cairocffi as cairo
+from PIL import Image, ImageOps
+from weasyprint import HTML, CSS
+
+PAGE_STYLE = """
+@page {
+  width: 384px;
+  margin: 0;
+}
+body {
+  margin: 0;
+}
+"""
 
 
 class Context:
@@ -29,13 +37,27 @@ def on_connect(client, userdata, flags, rc):
     client.subscribe("printer/print")
 
 
-def is_emoji(s: str) -> bool:
-    count = 0
-    for emoji in UNICODE_EMOJI:
-        count += s.count(emoji)
-        if count > 1:
-            return False
-    return bool(count)
+def write_image_surface(doc, resolution=96):
+    dppx = resolution / 96
+
+    # This duplicates the hinting logic in Page.paint. There is a
+    # dependency cycle otherwise:
+    #   this → hinting logic → context → surface → this
+    # But since we do no transform here, cairo_context.user_to_device and
+    # friends are identity functions.
+    widths = [int(math.ceil(p.width * dppx)) for p in doc.pages]
+    heights = [int(math.ceil(p._page_box.children[0].height * dppx)) for p in doc.pages]
+
+    max_width = max(widths)
+    sum_heights = sum(heights)
+    surface = cairo.ImageSurface(cairo.FORMAT_ARGB32, max_width, sum_heights)
+    context = cairo.Context(surface)
+    pos_y = 0
+    for page, width, height in zip(doc.pages, widths, heights):
+        pos_x = (max_width - width) / 2
+        page.paint(context, pos_x, pos_y, scale=dppx, clip=True)
+        pos_y += height
+    return surface, max_width, sum_heights
 
 
 def convert_image(img: Image) -> Image:
@@ -49,29 +71,6 @@ def convert_image(img: Image) -> Image:
     im = ImageOps.invert(im)
     im = im.convert("1")
     return im
-
-
-def get_image(url: str) -> typing.Union[None, Image.Image]:
-    try:
-        data_uri = w3lib.url.parse_data_uri(url)
-        try:
-            im = Image.open(io.BytesIO(data_uri.data))
-            im.load()
-            return im
-        except IOError:
-            return None
-    except ValueError:
-        try:
-            response = requests.get(url)
-            response.raise_for_status()
-        except requests.exceptions.RequestException:
-            return None
-        try:
-            im = Image.open(io.BytesIO(response.content))
-            im.load()
-            return im
-        except IOError:
-            return None
 
 
 def print_image(im: Image.Image, printer: escpos.escpos.Escpos):
@@ -92,86 +91,16 @@ def print_image(im: Image.Image, printer: escpos.escpos.Escpos):
     printer._raw(b''.join(outp))
 
 
-def print_emoji(text: str, printer: escpos.escpos.Escpos):
-    im = Image.new('RGB', (24, 24), 'white')
-    draw = ImageDraw.Draw(im)
-    font = ImageFont.truetype('NotoEmoji-Regular.ttf', 18)
-    draw.text((0, 0), text, (0, 0, 0), font=font)
-    im = im.convert("L")
-    im = ImageOps.invert(im)
-    im = im.convert("1")
-    header = escpos.constants.ESC + b"*\x21" + struct.pack("<H", im.width)
-    im = im.transpose(Image.ROTATE_270).transpose(Image.FLIP_LEFT_RIGHT)
-    outp = [escpos.constants.ESC + b"3\x16"]  # Adjust line-feed size
-    im_bytes = im.tobytes()
-    outp.append(header + im_bytes)
-    outp.append(escpos.constants.ESC + b"2")  # Reset line-feed size
-    printer._raw(b''.join(outp))
-
-
-def print_text(text: str, printer: escpos.escpos.Escpos):
-    text = regex.findall(r'.\p{Sk}+|\X', text)
-    for char in text:
-        if len(char) > 1 or ord(char) > 127:
-            print_emoji(char, printer)
-        else:
-            printer.text(char)
-
-
-def walk_html_tree(node: etree.Element, printer: escpos.escpos.Escpos):
-    print(node.tag, node.attrib, node.text, node.tail)
-    print_text(str(node.text), printer)
-    for child in node:
-        if child.tag == "br":
-            print_text('\n', printer)
-        elif child.tag == "i" or child.tag == "em" or child.tag == "cite":
-            printer._raw(escpos.constants.ESC + b'\x34\x01')
-            walk_html_tree(child, printer)
-            printer._raw(escpos.constants.ESC + b'\x34\x00')
-        elif child.tag == "b":
-            printer._raw(escpos.constants.ESC + b'\x45\x01')
-            walk_html_tree(child, printer)
-            printer._raw(escpos.constants.ESC + b'\x45\x00')
-        elif child.tag == "u":
-            printer._raw(escpos.constants.ESC + b'\x2d\x01')
-            walk_html_tree(child, printer)
-            printer._raw(escpos.constants.ESC + b'\x2d\x00')
-        elif child.tag == "h1":
-            printer._raw(escpos.constants.ESC + b'\x21\x12')
-            printer._raw(escpos.constants.ESC + b'\x45\x01')
-            walk_html_tree(child, printer)
-            print_text('\n', printer)
-            printer._raw(escpos.constants.ESC + b'\x21\x00')
-            printer._raw(escpos.constants.ESC + b'\x45\x00')
-        elif child.tag == "h2":
-            printer._raw(escpos.constants.ESC + b'\x21\x11')
-            printer._raw(escpos.constants.ESC + b'\x45\x01')
-            walk_html_tree(child, printer)
-            print_text('\n', printer)
-            printer._raw(escpos.constants.ESC + b'\x21\x00')
-            printer._raw(escpos.constants.ESC + b'\x45\x00')
-        elif child.tag == "h3":
-            printer._raw(escpos.constants.ESC + b'\x21\x01')
-            printer._raw(escpos.constants.ESC + b'\x45\x01')
-            walk_html_tree(child, printer)
-            print_text('\n', printer)
-            printer._raw(escpos.constants.ESC + b'\x21\x00')
-            printer._raw(escpos.constants.ESC + b'\x45\x00')
-        elif child.tag == "img":
-            img = get_image(child.get('src'))
-            if img is None:
-                print_text(str(child.get('alt')), printer)
-            else:
-                img = convert_image(img)
-                print_image(img, printer)
-        else:
-            walk_html_tree(child, printer)
-    print_text(str(node.tail), printer)
-
-
 def parse_html(printer: escpos.escpos.Escpos, html: str):
-    tree = etree.HTML(html)
-    walk_html_tree(tree, printer)
+    doc = HTML(string=html).render(stylesheets=[CSS(string=PAGE_STYLE)], enable_hinting=True)
+    surface, _, _ = write_image_surface(doc)
+    target = io.BytesIO()
+    surface.write_to_png(target)
+    target.seek(0)
+    im = Image.open(target)
+    im.load()
+    im = convert_image(im)
+    print_image(im, printer)
 
 
 def on_message(client, userdata: Context, msg: mqtt.MQTTMessage):
@@ -216,7 +145,8 @@ def on_message(client, userdata: Context, msg: mqtt.MQTTMessage):
 
 
 if __name__ == "__main__":
-    printer = escpos.printer.Usb(0x0416, 0x5011)
+    # printer = escpos.printer.Usb(0x0416, 0x5011)
+    printer = escpos.printer.Dummy()
     printer._raw(escpos.constants.ESC + b'\x40')
 
     context = Context(printer)
